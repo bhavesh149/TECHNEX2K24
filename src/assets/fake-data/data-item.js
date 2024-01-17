6a8e5b1610845902d576ebfe06b1cf582c79ca90
bhavesh149 <mahajanbhaveshop2@gmail.com>
2024-01-17 22:50:46 +0530

    technex-3 first commit

---
+++ src/assets/fake-data/data-item.js
@@ -0,0 +1,58 @@
+
+import img1 from '../images/item/item-01.png'
+import img2 from '../images/item/item-02.png'
+import img3 from '../images/item/item-03.png'
+import img4 from '../images/item/item-04.png'
+import img5 from '../images/item/item-04.png'
+import img6 from '../images/item/item-03.png'
+import img7 from '../images/item/item-02.png'
+import img8 from '../images/item/item-01.png'
+
+
+
+const dataItem = [
+
+    {
+        id: 1,
+        img: img1,
+        title: 'Projectomania',
+    },
+    {
+        id: 2,
+        img: img2,
+        title: 'Robowars',
+    },
+    {
+        id: 3,
+        img: img3,
+        title: 'Hackthon',
+    },
+    {
+        id: 4,
+        img: img4,
+        title: 'Incubators',
+    },
+    {
+        id: 5,
+        img: img5,
+        title: 'Incubators',
+    },
+    {
+        id: 6,
+        img: img6,
+        title: 'Hackthon',
+    },
+    {
+        id: 7,
+        img: img7,
+        title: 'Robowars',
+    },
+    {
+        id: 8,
+        img: img8,
+        title: 'Projectomania',
+    },
+
+]
+
+export default dataItem;
